3817e72522e5d0e98fcf34fe927eb80d14bc34ff
Pawel Mendzik <mendzik.pawel@gmail.com>
2023-07-06 03:44:13 +0200

    Formatowanie zapisu // clearingDataText()

--- src/App.js
+++ src/App.js
@@ -120,10 +120,19 @@ function App() {
       )}
 }
 
+function clearingDataText(dataToClear) {
+  const text = JSON.stringify(dataToClear);
+  let readyText = text.replaceAll(/["{XY:]/g , "")
+  .replaceAll(/[,[]/g , " ")
+  .replaceAll("}" , "\n")
+  .replaceAll("]" , "");
+
+  return readyText
+}
+
 function copyData() {
-  const textData = renderData([selectedMethod]);
   const element = document.createElement("a");
-  const file = new Blob([textData], { type: "text/plain" });
+  const file = new Blob([clearingDataText(data)], { type: "text/plain" });
   element.href = URL.createObjectURL(file);
   element.download = "data.txt";
   document.body.appendChild(element);
